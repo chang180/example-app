ebabe11aab5c093023d9ba90cc9fa00ba42ba7e1
chang180 <chang180@gmail.com>
2022-06-06 10:33:29 +0800

    vue enabled

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,20 @@
 require('./bootstrap');
 
-import Alpine from 'alpinejs';
+// import Alpine from 'alpinejs';
 
-window.Alpine = Alpine;
+// window.Alpine = Alpine;
 
-Alpine.start();
+// Alpine.start();
+
+require('alpinejs');
+
+import { createApp } from 'vue';
+import router from './router';
+
+import CompaniesIndex from './components/companies/CompaniesIndex.vue';
+
+createApp({
+    components:{
+        CompaniesIndex
+    }
+}).use(router).mount('#app');
