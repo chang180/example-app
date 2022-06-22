50330bd31394c7e32b7e77b71e6fb8f2f03cb6b5
chang180 <chang180@gmail.com>
2022-06-22 17:24:14 +0800

    Lesson 1 finished,for set and design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,88 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>Laracasts Voting</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+
+<body class="font-sans bg-gray-background text-gray-900 text-sm">
+    <header class="flex items-center justify-between px-8 py-4">
+        <a href="#"><img src="{{ asset('img/logo.svg') }}" alt="logo"></a>
+        <div class="flex items-center">
+            @if (Route::has('login'))
+                <div class="px-6 py-4">
+                    @auth
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <a href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </a>
+                        </form>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log
+                            in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}"
+                                class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                        @endif
+                    @endauth
                 </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            @endif
+            <a href="#">
+                <img src="https://gravatar.com/avatar/4f8f12cb6965efe16f7d032afeea0a5a?d=mp" alt="avatar"
+                    class="w-10 h-10 rounded-full">
+            </a>
         </div>
-    </body>
+    </header>
+
+    <main class="container mx-auto flex max-w-custom">
+        <div class="w-70 mr-5">
+            Add idea form. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum eligendi sapiente facere
+            animi molestiae, odit quasi nemo ad possimus illo accusantium magnam laboriosam at tenetur neque hic fuga
+            error aliquam porro, praesentium eius! Quis odio sequi aut nemo minima fuga, distinctio quaerat magnam
+            sapiente adipisci tempore numquam? Impedit placeat laudantium in neque ut quod corrupti similique vero
+            quibusdam, ullam, nemo repellendus asperiores illum, alias perferendis? Eum, exercitationem. Non aut
+            distinctio harum ipsa, deleniti cupiditate expedita totam laboriosam recusandae quis commodi eveniet aperiam
+            fugiat cumque. Voluptate accusamus mollitia praesent ium commodi tempore distinctio, pariatur dolor
+            temporibus quasi est aliquid, repellendus sunt consectetur.
+        </div>
+        <div class="w-175">
+            <nav class="flex items-center justify-between text-xs">
+                <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                    <li><a href="#" class="border-b-4 pb-3 border-blue">All Ideas(87)</a></li>
+                    <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Considering(8)</a></li>
+                    <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">In Progress(18)</a></li>
+                </ul>
+                <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                    <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Implemented(10)</a></li>
+                    <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Closed(18)</a></li>
+                </ul>
+            </nav>
+
+            <div class="mt-8">
+                {{ $slot }}
+            </div>
+
+        </div>
+    </main>
+</body>
+
 </html>
